b093ac35f4e55e8ad2a309cd403f6c8bca42dc53
Jepooyyy <salazarjeffrey207@gmail.com>
2024-10-31 12:00:00 +0800

    for editing to infra

--- essential/connection_ext.php
+++ essential/connection_ext.php
@@ -1,9 +1,9 @@
 <?php
 
-$ext_hrmis_servername = "172.31.32.57";
-$ext_hrmis_username = "ERM";
-$ext_hrmis_password = "P@ssw0rd1.";
-$ext_hrmis_dbname = "registration";
+$ext_hrmis_servername = "localhost";
+$ext_hrmis_username = "root";
+$ext_hrmis_password = "";
+$ext_hrmis_dbname = "db_logs_registration";
 
 try {
     $conn_ext = new PDO("mysql:host=$ext_hrmis_servername;dbname=$ext_hrmis_dbname", $ext_hrmis_username, $ext_hrmis_password);
